f37ca50da511f31379d5957f865eb995c7a93ab0
brovnie <bronka1.dev@gmail.com>
2024-11-14 14:20:50 +0100

    fixed error async

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,10 +1,20 @@
 import Form from '@/components/form/Form';
+import ProfileForm from '@/components/form/ProfileForm';
 
-const SignUp = () => {
+interface SearchParams {
+  searchParams: {
+    step: string;
+  };
+}
+
+const SignUp = async ({ searchParams }: SearchParams) => {
+  const params = await searchParams;
+  const step = params.step;
   return (
     <div>
       <h2>Sign up</h2>
-      <Form mode={'signup'} />
+      {step === '1' && <Form mode={'signup'} />}
+      {step === '2' && <ProfileForm />}
     </div>
   );
 };
