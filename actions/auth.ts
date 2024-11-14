1f5a9c774114118099f09373306704a8770a0227
brovnie <bronka1.dev@gmail.com>
2024-11-14 17:35:19 +0100

    catch error whensing up

--- actions/auth.ts
+++ actions/auth.ts
@@ -9,10 +9,10 @@ import {
 export async function signup(prevState, formData: FormData) {
   const email = formData.get('email')?.toString();
   const password = formData.get('password')?.toString();
-  const errors = {};
+
   if ((email && !email.includes('@')) || (password && password.length < 8)) {
     return {
-      status: 400,
+      status: 422,
       error: {
         message: 'Invalid data',
       },
@@ -20,11 +20,25 @@ export async function signup(prevState, formData: FormData) {
   }
 
   if (email && password) {
-    createUserWithEmailAndPassword(auth_firebase, email, password);
-    return {
-      status: 200,
-    };
+    try {
+      await createUserWithEmailAndPassword(auth_firebase, email, password);
+      return {
+        status: 200,
+        email,
+      };
+    } catch (err) {
+      return {
+        status: 400,
+        error: {
+          message: 'User already exists',
+        },
+      };
+    }
   }
+}
+
+export async function saveProfile(prevState, formData: FormData) {
+  const userName = formData.get('username');
 }
 
 export async function login(prevState, formData: FormData) {
